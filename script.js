8b30bace14ea823f134eeac83453e083e049b808
Crazy-Co <khushdevyogi001@gmail.com>
2022-05-20 23:04:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+function change1() {
+    const i = document.getElementById('out');
+    i.textContent = "January";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide1() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change2() {
+    const i = document.getElementById('out');
+    i.textContent = "February";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide2() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change3() {
+    const i = document.getElementById('out');
+    i.textContent = "March";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide3() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change4() {
+    const i = document.getElementById('out');
+    i.textContent = "April";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide4() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change5() {
+    const i = document.getElementById('out');
+    i.textContent = "May";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide5() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change6() {
+    const i = document.getElementById('out');
+    i.textContent = "June";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide6() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change7() {
+    const i = document.getElementById('out');
+    i.textContent = "July";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide7() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change8() {
+    const i = document.getElementById('out');
+    i.textContent = "August";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide8() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+function change9() {
+    const i = document.getElementById('out');
+    i.textContent = "September";
+    i.style.transition = "all 0.2s ease";
+    i.style.transform = "scale(1.1)";
+}
+function hide9() {
+    const j = document.getElementById('out');
+    j.textContent = "LOOK AT ME";
+    j.style.transition = "all 0.5s ease-out";
+    j.style.transform = "scale(1)";
+}
+var loader = document.getElementById("preloader");
+window.addEventListener(setInterval(function ()
+{ loader.style.display = "none" }, 3000))
